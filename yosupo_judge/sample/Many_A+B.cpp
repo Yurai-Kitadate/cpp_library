876e79904ad506839299006f94229c7e30a13a20
tk <82942294+Yurai-Kitadate@users.noreply.github.com>
2021-11-25 15:42:42 +0900

    Create Many_A+B.cpp

---
+++ yosupo_judge/sample/Many_A+B.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int T;
+	cin >> T;
+	long long A, B;
+	for (int i = 0; i < T; i++)
+	{
+		scanf("%lld %lld",&A,&B);
+		printf("%lld\n",A + B);
+	}
+}
